43f6cd71620d22317503ae3d3a9acb334dc45ead
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-24 13:13:55 +0530

    Initial commit with frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    //root route http://localhost:5000/
+    res.send('Hello world !!!!!!!')
+})
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`)) 
