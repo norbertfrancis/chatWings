815cb768eb658f4eb4787293ed72bacffb64d189
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-25 16:24:12 +0530

    allmost completed the backend except setting the socketio

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,8 +2,8 @@ export const getUsersForSiderbar = async (req, res) => {
     try {
        const loggedInUserId = req.user._id;
        
-       const filteredUsers = await User.find({ _id:{$ne: loggedInUserId}})
-       
+       const filteredUsers = await User.find({ _id:{$ne: loggedInUserId}}).select("-password")
+
       res.status(200).json(filteredUsers)  
     } catch (error) {
         console.log("Error in getUsersForSiderbar:", error.message);
