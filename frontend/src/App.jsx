f641e9ee68a0f68090f1211c66de5986ecbc7473
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-26 16:52:30 +0530

    set the login componenet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,12 @@
+import Login from "./pages/login/Login"
+
+
 function App() {
-  return (
-    <>
-  
-      <div className="badge badge-primary">Test Badge</div>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
-    </>
-  );
+  return <div className="p-4 h-screen flex items-center justify-center">
+    
+    <Login/>
+
+    </div>
 }
 
 export default App;
---
+++ frontend/src/pages/signup/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>
+      SignUp  Page
+    </div>
+  )
+}
+
+export default SignUp
+
