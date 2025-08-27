3c0c65b7272bcfb4a6155c8ced11247e66a64702
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-27 17:34:25 +0530

    just started Home comp, and  for sidebar component , build SearchInput comp, Conversations and conversation comp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login"
 import SignUp from "./pages/signup/SignUp";
 
@@ -6,7 +7,7 @@ import SignUp from "./pages/signup/SignUp";
 function App() {
   return <div className="p-4 h-screen flex items-center justify-center">
     
-    <SignUp/>
+    <Home/>
 
     </div>
 }
