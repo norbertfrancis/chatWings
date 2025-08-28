7fb92bb6dc17b8b7993095744799d3c8e838ad35
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-28 10:24:16 +0530

    add NoChatSelected component to the MessageContainer component

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -16,9 +16,34 @@ const Messages = () => {
         <Message/>
         <Message/>
         
-      
     </div>
   )
 }
 
 export default Messages
+
+
+// STARTER CODE SNIPPET 
+// import Message from "./Message"
+
+// const Messages = () => {
+//   return (
+//     <div className="px-4 flex-1 overflow-auto">
+//         <Message/>
+//         <Message/>
+//         <Message/>
+//         <Message/>
+//         <Message/>
+//         <Message/>
+//         <Message/>
+//         <Message/>
+//         <Message/>
+//         <Message/>
+//         <Message/>
+//         <Message/>
+        
+//     </div>
+//   )
+// }
+
+// export default Messages
