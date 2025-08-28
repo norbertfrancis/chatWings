a60188b6f23c9cac97be2027293f6865c91eb521
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-28 18:15:38 +0530

    created useContext to navigate the auth user to all components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import {Toaster} from 'react-hot-toast'
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
+  const {authUser} = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={authUser ? <Navigate to='/' /> : <SignUp/>} />
       </Routes>
       <Toaster/>
     </div>
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
     const [loading, setLoading] = useState(false)
+    const {setAuthUser} = useAuthContext();
 
     const signup = async({fullName, username, password ,confirmPassword, gender}) => {
         const success = handleInputError({fullName, username, password,confirmPassword,gender});
@@ -15,7 +17,11 @@ const useSignup = () => {
                 body:JSON.stringify({fullName, username, password, confirmPassword, gender})
             });
             const data = await res.json()
-            console.log(data)
+            if(data.error) {
+                throw new Error(data.error)
+            }
+            localStorage.setItem("chat-user",JSON.stringify(data))
+            setAuthUser(data)
  
         } catch (error) {
             toast.error(error.message);
