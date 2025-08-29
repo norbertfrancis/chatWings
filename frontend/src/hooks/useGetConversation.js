4ebe6a956dce351ffd1e9b4cd4f4028f17c4b40e
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-29 16:05:42 +0530

    for creating global context for conversation , use ZUSTAND and created useConversation.js   , then created another custom hook to fetch conversations from the DB

---
+++ frontend/src/hooks/useGetConversation.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+
+const useGetConversation = () => {
+    const [loading, setLoading] = useState(false);
+    const [conversation, setConversation] = useState([])
+
+    useEffect(() => {
+        const getConversations = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch('/api/users');
+                const data = await res.json();
+                if(data.error){
+                    throw new Error(data.error)
+                } 
+                setConversation(data)       
+            } catch (error) {
+                toast.error(error.message)
+            }finally {
+                setLoading(false)
+            }
+        };
+        getConversations();
+    },[]);
+    return {loading, conversation};
+}
+export default useGetConversation
