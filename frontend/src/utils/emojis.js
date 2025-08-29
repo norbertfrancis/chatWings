e0f926f5ced82c5ca3aeba4022c6fb19e8bb5498
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-29 22:18:11 +0530

    correct random emoji and loading spinner condition at Conversation.jsx

--- frontend/src/utils/emojis.js
+++ frontend/src/utils/emojis.js
@@ -5,5 +5,5 @@ export const funEmojis = [
 ];
 
 export const getRandomEmojis = () => {
-    return funEmojis[Math.floor(Math.random())*funEmojis.length]
+    return funEmojis[Math.floor(Math.random()* funEmojis.length)]
 };
