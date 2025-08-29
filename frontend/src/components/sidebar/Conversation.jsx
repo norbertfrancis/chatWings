822db4d9ea60102980cc557602d035410b664afc
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-29 22:46:39 +0530

    add sidebar user selection color bg-sky-400

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,8 +1,15 @@
+import useConversation from "../../zustand (store)/useConversation";
 
 const Conversation = ({conversation,lastIdx,emoji}) => {
+  const {selectedConversation,setSelectedConversation} = useConversation(); 
+  
+  const isSelected = selectedConversation?._id === conversation._id;
+
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+      <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${isSelected ? "bg-sky-400" : " "}`
+    }onClick={() => setSelectedConversation(conversation)}
+    >
         <div className="avatar online">
           <div className="w-12 rounded-full">
             <img
