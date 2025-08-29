541c0e3398e2042ca6f214981062082c81fa62f0
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-29 11:11:56 +0530

    set clear  Route path for Home and Login in App.jsx  and Set the Logout functionality by creating useLogout hook and applied in LogoutButton comp

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,10 +1,18 @@
 import {BiLogOut} from 'react-icons/bi'
+import useLogut from '../../hooks/useLogout';
 
 const LogoutButton = () => {
+  const {loading, logout} = useLogut();
+
   return (
     <div className='mt-auto'>
-      <BiLogOut className="w-6 h-6 text-white cursor-pointer"/>
-    </div>
+      {!loading ? (
+        <BiLogOut className="w-6 h-6 text-white cursor-pointer" onClick={logout}/>
+      ):(
+        <span className='loading loading-spinner'></span>
+      )
+      }
+          </div>
   )
 };
 
