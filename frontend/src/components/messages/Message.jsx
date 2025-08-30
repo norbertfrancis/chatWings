93134328be7f06f440a383ece227f2d6b457ef42
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-30 14:43:51 +0530

    add scroll functionality and extract time properly

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,16 +1,15 @@
 import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand (store)/useConversation";
 
 const Message = ({message}) => {
   const {authUser} = useAuthContext();
   const {selectedConversation } = useConversation();
   const fromMe = message.senderId === authUser._id;
+  const formatTime = extractTime(message.createdAt);
   const chatClassName = fromMe ? 'chat-end' : 'chat-start';
   const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
   const bubbleBgColor = fromMe ? 'bg-blue-500' : "";
-
-
-
 
   return (
     <div className={`chat ${chatClassName}`}>
@@ -22,8 +21,8 @@ const Message = ({message}) => {
           />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500 ${bubbleBgColor} `}>{message.message}</div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">12:42 </div>
+      <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formatTime}</div>
     </div>
   );
 };
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+// import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
 
 const SearchInput = () => {
+  // const [search, setSearch] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
   return (
-    <form className="flex items-center gap-2">
+    <form onSubmit={handleSubmit} className="flex items-center gap-2">
       <input
         type="text"
         placeholder="Search..."
