d546729b3aec9da417feb6f8fd67ef72bcf18699
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-30 15:10:17 +0530

    add search fuctionality in SearchInput component

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,10 +1,26 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustand (store)/useConversation";
+import useGetConversations from "../../hooks/useGetConversation";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
-  // const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("");
+  const {setSelectedConversation} = useConversation()
+  const { conversations } = useGetConversations()
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(!search) return 
+    if(search.length < 3){
+      return toast.error(`Search term must be at least 3 chatecters`)
+    }
+    const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+
+    if(conversation) {
+      setSelectedConversation(conversation)
+      setSearch('');
+    }else toast.error("No such user found")
 
   }
   return (
@@ -13,6 +29,8 @@ const SearchInput = () => {
         type="text"
         placeholder="Search..."
         className="input input-borderedn rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button type="submit" className="btn btn-circle bg-sky-500 text-white">
         <IoSearchSharp className="w-6 h-6 outline-none" />
