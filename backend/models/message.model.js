05e7e6586d12780bce5ffcbf950251eb412f3cb1
norbertfrancis <norbertfrancis07@gmail.com>
2025-08-30 12:32:59 +0530

    create new MessageSkelton and updated messageinput

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -19,4 +19,4 @@ const messageSchema = new mongoose.Schema({
 
 const Message = mongoose.model("Message", messageSchema);
 
-export default Message;
+export default Message; 
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,23 +1,17 @@
+import useGetMessages from "../../hooks/useGetMessages"
+import MessageSkeleton from "../skeletons/MessageSkeleton";
+
 import Message from "./Message"
 
 const Messages = () => {
+  const {messages,loading} = useGetMessages();
+  console.log("messages:",messages);
   return (
     <div className="px-4 flex-1 overflow-auto">
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
+       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} /> ) }
         
     </div>
-  )
+  )  
 }
 
 export default Messages
